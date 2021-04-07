8eac3e455fe7ca657513b628b91ab236ac313c6e
Chan <ieejo716@naver.com>
2021-04-07 17:37:11 +0900

    test

--- clone/src/App.js
+++ clone/src/App.js
@@ -1,20 +1,25 @@
-import './mainPage/webHeader.css'
-import './mainPage/webBody.css'
-import './mainPage/webList'
-import './App.css'
-import './waitingRoom/waitingRoom.css'
+// import './mainPage/webHeader.css'
+// import './mainPage/webBody.css'
+// import './mainPage/webList'
+// import './App.css'
+// import './waitingRoom/waitingRoom.css'
+// import {Switch, Route, useHistory} from "react-router-dom"
+// import WebHeader from './mainPage/webHeader'
+// import WebBody from './mainPage/webBody'
+// import {BrowserRouter as Router} from 'react-router-dom';
+// import WaitingRoom from './waitingRoom/waitingRoom'
+// import CreateRoom from './createRoom/createRoom'
 
-import WebHeader from './mainPage/webHeader'
-import WebBody from './mainPage/webBody'
-import WaitingRoom from './waitingRoom/waitingRoom'
+import React from 'react'
+import Button from '@material-ui/core/Button'
+import ButtonAPI from './notepad/lab/buttonAPI'
 
 function App() {
-  return (
-    <div className="App">
-        <WebHeader/>
-        <WebBody/>
-    </div>
-  );
+    return (
+        <div>
+            <ButtonAPI/>
+        </div>
+    );
 }
 
-export default App;
+export default App
