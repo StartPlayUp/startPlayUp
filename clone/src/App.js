e7a7d3c84b8cee103d46cb8edc654e57b2dca5c2
Chan <ieejo716@naver.com>
2021-04-07 22:21:15 +0900

    styled-component apply

--- clone/src/App.js
+++ clone/src/App.js
@@ -1,25 +1,20 @@
-// import './mainPage/webHeader.css'
-// import './mainPage/webBody.css'
-// import './mainPage/webList'
-// import './App.css'
-// import './waitingRoom/waitingRoom.css'
-// import {Switch, Route, useHistory} from "react-router-dom"
-// import WebHeader from './mainPage/webHeader'
-// import WebBody from './mainPage/webBody'
-// import {BrowserRouter as Router} from 'react-router-dom';
-// import WaitingRoom from './waitingRoom/waitingRoom'
-// import CreateRoom from './createRoom/createRoom'
+import './mainPage/webList'
+import './App.css'
+import {Switch, Route, useHistory} from "react-router-dom"
+
+import WebHeader from './mainPage/webHeader'
+import WebBody from './mainPage/webBody'
 
 import React from 'react'
-import Button from '@material-ui/core/Button'
-import ButtonAPI from './notepad/lab/buttonAPI'
 
 function App() {
     return (
         <div>
-            <ButtonAPI/>
+            <WebHeader/>
+            <WebBody/>
         </div>
     );
 }
+
 
 export default App
