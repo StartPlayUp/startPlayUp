1bbebd631b894be8aed73c9a2521f2ea4a6d6a86
Chan <ieejo716@naver.com>
2021-04-07 22:20:11 +0900

    React-styled-Component apply

--- clone/src/mainPage/webBody.js
+++ clone/src/mainPage/webBody.js
@@ -1,39 +1,80 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
-function webBody() {
+const BodyFrame = styled.div`
+    width: 100%;
+    height: 100%;
+    position : absolute;
+`;
+const BodyCenter = styled.div`
+    width: 75%;
+    height: 90%;
+    margin: 0 auto;
+    flex: 1 1 auto;
+    flex-direction: column;
+`;
 
+const RoomList = styled.div`
+    height: 90%;
+    background-color: #FFFFF3;
+    flex: 1 1 auto;
+    flex-direction: column;
+`;
+
+const UserList = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    padding: 5px;
+`;
+
+const Users = styled.span`
+    margin: 0 auto;
+    font-size: 18px;
+    
+
+    &.title{
+        font-size : 24px;
+    }
+`;
+
+const ButtonArea = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 30px;
+`;
+
+const Button = styled.button`
+    font-size: 14px;
+    color: #FFFFFF;
+    background-color: #A593E0;
+    border-radius: 2px;
+    border: 0;
+    padding: 10px;
+`;
+
+const BODY = ({children}, ...rest) => {
     return (
-        <div id="body">
-            <div id={"body-center"}>
-                <div id = "button-area">
-                    <button>
-                        Create Room
-                    </button>
-                </div>
-                <div id="room-list">
-                     <div className="list">
-                         <span>방 번호</span>
-                         <span className="title">방 제목</span>
-                         <span>사용자 </span>
-                         <span>인원 수</span>
-                     </div>
-                    <hr/>
-                     <div className="list">
-                         <span>방 번호</span>
-                         <span className="title">방 제목</span>
-                         <span>사용자 </span>
-                         <span>인원 수</span>
-                     </div>
-                </div>
-            </div>
-            <div id="footer-area">
-                <h1>footer-area</h1>
-                <div id="footer-center">
-                    footer-center
-                </div>
-            </div>
+        <div>
+            <BodyFrame>
+                <BodyCenter>
+                    <ButtonArea>
+                        <Button>Create Room</Button>
+                    </ButtonArea>
+                    <RoomList>
+                        <UserList>
+                            <Users>방 번호</Users>
+                            <Users>방 제목</Users>
+                            <Users>방 사용자</Users>
+                            <Users>인원</Users>
+                        </UserList>
+                        <UserList>
+                            {/*    map 사용하여 정렬하기 추후에 선도 그러놓고 할 거임 일단 틀만 잡아놈*/}
+                        </UserList>
+                    </RoomList>
+                </BodyCenter>
+            </BodyFrame>
         </div>
-    );
+    )
 }
 
-export default webBody;
+export default BODY;
--- clone/src/mainPage/webHeader.js
+++ clone/src/mainPage/webHeader.js
@@ -1,18 +1,49 @@
+import React from "react";
+import styled from "styled-components";
 
-function head() {
+
+const HeadStyle = styled.div`
+    width: 75%;
+    height: 100%;
+    margin : 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const HeadColor = styled.div`
+    background : #566270;
+`;
+
+const HeadLeft = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    color : white;
+    font-weight: lighter;
+    font-size : ${(props) => props.fontSize};
+`;
+
+const HeadRight = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    font-size : ${(props) => props.fontSize};
+`
+
+const HEADER = ({children, ...rest}) => {
     return (
-        <div id="header">
-            <div id="header-area">
-                <div id="header-area-start">
-                    <img src="images/startplayup.png" alt="logo"/>
-                </div>
-                <div id="header-area-end">
-                    <img src="images/white-message.png" alt="message"/>
-                    <img src="images/white-menu.png" alt="message"/>
-                </div>
-            </div>
+        <div>
+            <HeadColor>
+                <HeadStyle>
+                    <HeadLeft fontSize={"32px"}>
+                        StartPlayUp
+                    </HeadLeft>
+                    <HeadRight fontSize = {"14px"}>
+                        icons
+                    </HeadRight>
+                </HeadStyle>
+            </HeadColor>
         </div>
-    );
+    )
 }
 
-export default head;
+export default HEADER;
