ef2ed04da7c1fce41267583e061a3c63bace4b5b
TypingOK <wjdwls717@gmail.com>
2021-04-16 00:06:50 +0900

    4.16

--- src/Jin/Auth/Main.js
+++ src/Jin/Auth/Main.js
@@ -2,9 +2,21 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 
 const Main = () =>{
+    const naver = new naver.LoginWithNaverId({
+        clientId: 'ZLFAGtItFGDqMKyhBgU9',
+        access_token: naver.oauthParams.access_token
+    })
+    console.log(naver.oauthParams.access_token)
+    naver.get_naver_userprofile("naverSignInCallback()");
+    // 네이버 사용자 프로필 조회 이후 프로필 정보를 처리할 callback function
+    const naverSignInCallback =() =>{
+        console.log(naver_id_login.getProfileData('email'));
+        console.log(naver_id_login.getProfileData('nickname'));
+    }
+    
     return(
         <div>
-            <h2>로그인 화면이 될 예정입니다. react router 연습용.</h2>
+            
         </div>
     );
 };
--- src/Jin/LoginForm.js
+++ src/Jin/LoginForm.js
@@ -1,10 +1,13 @@
 import React,{useState,useContext, Fragment, useEffect} from 'react';
 import styled from 'styled-components';
-import {Route} from 'react-router-dom';
+import {Route, useHistory,useLocation} from 'react-router-dom';
 import { Main } from './Auth';
 import { AuthStore, AuthProvider} from './Auth/AuthContext';
 import { signInWithGoogle} from '../util/firebase_config';
 import { getSuggestedQuery } from '@testing-library/dom';
+import firebase from 'firebase';
+import KakaoLogin from "react-kakao-login";
+import NaverLogin from "react-login-by-naver";
 
 
 const Button = styled.button`
@@ -27,8 +30,10 @@ const Input = styled.input`
     
 `
 
-function LoginForm(){
+function LoginForm(){ 
     const AuthCon = useContext(AuthStore);
+    const history=useHistory()
+    const location=useLocation();
     const [inputs, setInputs] = useState({
         email: '',
         password: ''
@@ -46,47 +51,6 @@ function LoginForm(){
         AuthCon.onLogin(inputs)
         
     };
-    const {naver} = window;
-    const NLogin=()=>{
-        initializeNaverLogin();
-        UserProfile();
-    }
-    const initializeNaverLogin=()=>{
-        const naverLogin=new naver.LoginWithNaverId({
-            clientId:'ZLFAGtItFGDqMKyhBgU9',
-            callbackUrl:'http://localhost:3000/Main',
-            isPopup:false,
-            loginButton: { color: "green", type: 3, height: 40 }
-        });
-        naverLogin.init();
-    }
-    useEffect(()=>{
-        initializeNaverLogin();
-    },[]);
-    const UserProfile=()=>{
-        window.location.href.includes('access_token')&&GetUser();
-        function GetUser(){
-            const location = window.location.hreft.split('=')[1];
-            const token=location.split('&')[0];
-            console.log("token: ", token);
-            fetch(`${API}/account/sign-in`, {
-                method: "GET",
-                headers: {
-                    "Content-type": "application/json",
-                    "Authorization": token
-                },
-            })
-                .then(res => res.json())
-                .then(res => {
-                    localStorage.setItem("access_token", res.token);
-                    setUserData({
-                        email:res.email,
-                        nickname: res.nickname,
-                    })
-                })
-                .catch(err => console.log("err : ", err));
-        }
-    };
     return(
         <AuthStore.Consumer>
         {({checkAuth})=>(
@@ -97,7 +61,20 @@ function LoginForm(){
                 <Input name="password" onChange={onChange} value = {password}/>
                 <Button onClick={logOn} >Login</Button>
                 <button onClick={signInWithGoogle}>구글로 로그인하기</button>
-                <div onClick={NLogin} id='naverIdLogin'/>
+                <NaverLogin
+                    clientId="ZLFAGtItFGDqMKyhBgU9"
+                    callbackUrl="http://localhost:3000/Main"
+                    render={(props) => <div onClick={props.onClick}>Naver Login</div>}
+                    onSuccess={(naverUser) => console.log(naverUser)}
+                    onFailure={() => console.error(result)}
+                ></NaverLogin>
+                <KakaoLogin
+                    jsKey={"3eba9d0656f87b8cb00ae9e1e9f6ca31"}
+                    onSuccess={(res) => console.log(res)}
+                    onFailure={(res) => console.log(res)}
+                    getProfile={true}
+                ><img src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg" /></KakaoLogin>
+                
             </form>
             )
         )
@@ -107,8 +84,70 @@ function LoginForm(){
     );
 
 }
+//네이버 로그인 <div onClick={initializeNaverLogin} id='naverIdLogin'/>
+    //네이버 로그인
+/*
+const {naver} = window;
+const initializeNaverLogin=()=>{
+    const naverLogin=new naver.LoginWithNaverId({
+        clientId:'ZLFAGtItFGDqMKyhBgU9',
+        callbackUrl:'http://localhost:3000/Main',
+        isPopup:false,
+        loginButton: { color: "green", type: 3, height: 40 }
+    });
+    naverLogin.init();
+}
+const getNaverToken=()=>{
+    if(!location.hash) return;
+    const token = location.hash.split('=')[1].split('&')[0];
+    console.log('로그인 성공',token);
+    console.log(window)
+};
+useEffect(() => {
+    initializeNaverLogin();
+    getNaverToken();
 
+}, []);
+*/
 export default LoginForm;
+ //카카오 로그인
+    /*
+    const KakaoLogin = () => {
+        
+        loginWithKakao();
+     }
+    
+    const loginWithKakao=()=>{
+        Kakao.init('3eba9d0656f87b8cb00ae9e1e9f6ca31');
+        try{
+            return new Promise((resolve,reject)=>{
+                if(!Kakao){
+                    reject("Kakao 인스턴스가 존재하지 않습니다.")
+                    }
+                    Kakao.Auth.login({
+                        success:(auth)=>{
+                            console.log('정상적으로 로그인 되었습니다.',auth)
+                            .then(res=> res.json())
+                            .then(res=>{
+                                localStorage.setItem("Kakao_token",res.access_token);
+                                if(res.access_token){
+                                    console.log(res)
+                                    history.push("/Main");
+                                }
+                            })
+                        },
+                        fail:(err)=>{
+                            console.error(err)
+                        }
+                    })
+                })
+            }
+            catch(err){
+                console.error(err)
+            }
+        }
+        */
+//<button onClick={KakaoLogin}><img src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg" /></button>
         /*
     const onReset=()=>{
         setInputs({
@@ -118,6 +157,7 @@ export default LoginForm;
     };
     */
    /*
+   
     return(
         
         <p>
