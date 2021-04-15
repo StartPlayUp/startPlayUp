4d7cc440d90600be06623590a67accbade8c50fe
Chan <ieejo716@naver.com>
2021-04-15 17:48:28 +0900

    web Style Frame

---
+++ clone/src/WebPage/WebFrameStyle.js
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+const HeadStyle = styled.div`
+    width: 75%;
+    height: 100%;
+    margin : 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const HeadColor = styled.div`
+    background : #566270;
+`;
+
+const HeadLeft = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    color : white;
+    font-weight: lighter;
+    font-size : ${(props) => props.fontSize};
+`;
+
+const HeadRight = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`
+const BodyFrame = styled.div`
+    width: 100%;
+    height: 100%;
+    background-color : #E0E3DA;
+    margin : 0 auto;
+    position : absolute;
+`;
+const BodyCenter = styled.div`
+    width: 75%;
+    height: 90%;
+    margin: 0 auto;
+    flex: 1 1 auto;
+    background-color = gray;
+    flex-direction: column;
+`;
+
+const RoomFrame = styled.div`
+    height: 90%;
+    background-color: #FFFFF3;
+    flex: 1 1 auto;
+    flex-direction: column;
+`;
+
+const UserList = styled.div`
+    display :fix;
+    padding: 5px;
+    color : ${props => props.color};
+    background-color : ${props => props.background};
+`;
+
+const Users = styled.span`
+    font-size : 14px;
+    width : ${props => props.width};
+    float : left;
+    
+`;
+
+const ButtonArea = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 30px;
+`;
+
+export default {
+    HeadColor,
+    HeadStyle,
+    HeadLeft,
+    HeadRight,
+    BodyFrame,
+    BodyCenter,
+    RoomFrame,
+    UserList,
+    Users,
+    ButtonArea
+}
