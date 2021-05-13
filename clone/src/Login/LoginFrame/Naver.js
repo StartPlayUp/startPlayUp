ec261edbf4520ab62ca88f6ff6718c90c67f7a06
Chan <ieejo716@naver.com>
2021-05-13 12:10:30 +0900

    05-13 이미지 파일 경로 수정

--- clone/src/Login/LoginFrame/Naver.js
+++ clone/src/Login/LoginFrame/Naver.js
@@ -1,4 +1,4 @@
-// const client_ID = ZLFAGtItFGDqMKyhBgU9
-// const client_secret = U2tNwFv1Th
-//
-// const 카카오=3eba9d0656f87b8cb00ae9e1e9f6ca31
+const client_ID = HI9KL4ilNF_j6vWKWQ8P
+const client_secret = 3qcqEpQDQI
+
+const 카카오=3eba9d0656f87b8cb00ae9e1e9f6ca31
