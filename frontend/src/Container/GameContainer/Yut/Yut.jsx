86fa897b45ae05e6ff9fe52b4750945971fdebea
JSC <gbs04087@gmail.com>
2021-07-31 21:44:43 +0900

    left split YutPlayserSection

--- frontend/src/Container/GameContainer/Yut/Yut.jsx
+++ frontend/src/Container/GameContainer/Yut/Yut.jsx
@@ -1,11 +1,26 @@
 import React, { useReducer } from 'react';
 import styled from 'styled-components';
-import Yutfield from 'Component/GameComponent/Yut/Yutfield'
+import Yutfield from 'Component/GameComponent/Yut/YutfieldSection'
 import YutPlayersSection from 'Component/GameComponent/Yut/YutPlayersSection';
 import YutStore from './YutStore';
-import YutAnimation from 'Component/GameComponent/Yut/YutAnimation';
+import YutAnimation from 'Component/GameComponent/Yut/YutAnimationSection';
 import WinnerModal from './WinnerModal';
 
+const StyleDivFlexRow = styled.div`
+    display:flex;
+    flex-direction: row;
+    width: inherit;
+    height:inherit;
+`;
+
+const StyledYutAnimation = styled(YutAnimation)`   
+    flex-grow: 7;
+
+`;
+
+const StyledYutfield = styled(Yutfield)`
+    flex-grow: 4.5;
+`;
 
 const Yut = (props) => {
     const StyleDiv = styled.div`
@@ -34,12 +49,8 @@ const Yut = (props) => {
         filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
     `;
 
-    const StyleDivFlexRow = styled.div`
-        display:flex;
-        flex-direction: row;
-        width: inherit;
-        height:inherit;
-    `;
+
+
 
     // const [value, setValue] = useReducer((state) => state + 1, 0); // integer state
     // return () => setValue(value => value + 1); // update the state to force render
@@ -49,8 +60,8 @@ const Yut = (props) => {
             <WinnerModal />
             <StyleDiv>
                 <StyleDivFlexRow>
-                    <Yutfield />
-                    <YutAnimation />
+                    <StyledYutfield />
+                    <StyledYutAnimation />
                 </StyleDivFlexRow>
                 <YutPlayersSection />
             </StyleDiv>
