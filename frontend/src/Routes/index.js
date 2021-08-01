d143acf5dd7a817daa29c40e9a1f12ca6431695f
Chan <ieejo716@naver.com>
2021-08-01 17:48:09 +0900

    React NODE_PATH=src

--- frontend/src/Routes/index.js
+++ frontend/src/Routes/index.js
@@ -1,48 +1,48 @@
-import React, { useContext } from "react";
-import { UserContext } from "../LoginStore"
-import { Route, Switch, useHistory } from 'react-router-dom';
+import React, {useContext} from "react";
+import {UserContext} from "../LoginStore"
+import {Route, Switch, useHistory} from 'react-router-dom';
 import LoginPageContainer from "Container/LoginPageContainer";
 import GlobalContainer from "Container/GlobalContainer";
 import ChatComponent from "Component/ChatComponent";
 import RockPaperScissors from "Component/GameComponent/RockPaperScissors";
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import styled from 'styled-components';
 import MineSearch from "Container/GameContainer/MineSearch";
 import Yut from 'Container/GameContainer/Yut/Yut';
 import Yacht from 'Container/GameContainer/Yacht/Yacht';
-import { PeerStore } from './peerStore';
+import {PeerStore} from './peerStore';
 import AVALON_BETA from "../Component/GameComponent/AVALON_BETA/AVALON_BETA";
 import View from "../Component/GameComponent/AVALON_BETA/View";
 
 const GamePage = styled.div`
-    display:flex;
-    flex-direction:row;
-    justify-content: center;
-    margin:10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 10px;
 `;
 
 const StyleDiv = styled.div`
-        display: flex;
-        background-color: #e0e3da;
-        justify-content: center;
-        flex-direction: row;
-        width: 100%;
-        min-height: 100vh;
+  display: flex;
+  background-color: #e0e3da;
+  justify-content: center;
+  flex-direction: row;
+  width: 100%;
+  min-height: 100vh;
 `
 const AVALON_STYLE = styled.div`
-    display : flex;
-    flex-basis : 70%;
-    flex-direction:column;
-    justify-content: flex-start;
-    flex-wrap : wrap;
-    background-color : #FFFFFF;
+  display: flex;
+  flex-basis: 70%;
+  flex-direction: column;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  background-color: #FFFFFF;
 `;
 
 const CHAT_STYLE = styled.div`
-    display:flex;
-    flex-basis:30%;
-    flex-direction:row;
-    justify-content:flex-end;
+  display: flex;
+  flex-basis: 30%;
+  flex-direction: row;
+  justify-content: flex-end;
 `;
 
 const TemporaryMain = () => {
@@ -57,49 +57,49 @@ const TemporaryMain = () => {
 }
 
 const app = () => {
-    const { isAuthenticated } = useContext(UserContext);
+    const {isAuthenticated} = useContext(UserContext);
     console.log("asdf")
     return (
         <BrowserRouter>
             {/* 원래 코드 주석 처리 ( 로그인 표시 X) */}
-            <Route path="/" component={GlobalContainer} />
-            <Route exact path="/" component={LoginPageContainer} />
-            {isAuthenticated && <Route exact path="/main" component={TemporaryMain} />}
+            <Route path="/" component={GlobalContainer}/>
+            <Route exact path="/" component={LoginPageContainer}/>
+            {isAuthenticated && <Route exact path="/main" component={TemporaryMain}/>}
             {isAuthenticated && <Route exact path="/RockPaperScissors" render={() => <GamePage>
                 <PeerStore>
-                    <RockPaperScissors />
-                    <ChatComponent />
+                    <RockPaperScissors/>
+                    <ChatComponent/>
                 </PeerStore>
-            </GamePage>} />}
+            </GamePage>}/>}
             {isAuthenticated && <Route exact path="/MineSearch" render={() => <GamePage>
                 <PeerStore>
-                    <MineSearch />
+                    <MineSearch/>
                 </PeerStore>
-            </GamePage>} />}
+            </GamePage>}/>}
             {isAuthenticated && <Route exact path="/Yut" render={() => <GamePage>
                 <PeerStore>
-                    <Yut />
-                    <ChatComponent />
+                    <Yut/>
+                    <ChatComponent/>
                 </PeerStore>
-            </GamePage>} />}
+            </GamePage>}/>}
             {isAuthenticated && <Route exact path="/Yacht" render={() => <GamePage>
                 <PeerStore>
                     <StyleDiv>
-                        <Yacht />
+                        <Yacht/>
                     </StyleDiv>
-                    <ChatComponent />
+                    <ChatComponent/>
                 </PeerStore>
-            </GamePage>} />}
+            </GamePage>}/>}
             {isAuthenticated && <Route exact path="/AVALON" render={() => <GamePage>
                 <PeerStore>
                     <AVALON_STYLE>
-                        <AVALON_BETA />
+                        <AVALON_BETA/>
                     </AVALON_STYLE>
                     <CHAT_STYLE>
-                        <ChatComponent />
+                        <ChatComponent/>
                     </CHAT_STYLE>
                 </PeerStore>
-            </GamePage>} />}
+            </GamePage>}/>}
         </BrowserRouter>
     );
 }
