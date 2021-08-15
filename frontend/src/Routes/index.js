fef9d79bb8783dbcf24a96ddff5117792c576239
Chan <ieejo716@naver.com>
2021-08-15 19:44:12 +0900

    AVALON_BETA

--- frontend/src/Routes/index.js
+++ frontend/src/Routes/index.js
@@ -32,8 +32,7 @@ const StyleDiv = styled.div`
 `;
 const AVALON_STYLE = styled.div`
   display: flex;
-  width: 100%;
-  height: 100%;
+  flex-basis: 70%;
   flex-direction: column;
   justify-content: flex-start;
   flex-wrap: wrap;
