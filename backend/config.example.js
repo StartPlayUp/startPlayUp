8ee5c434beb4bec21e2771e24dadddeb850f7fef
JSC <gbs04087@gmail.com>
2021-09-19 23:31:38 +0900

    test

--- backend/config.example.js
+++ backend/config.example.js
@@ -3,18 +3,11 @@ module.exports = {
     'secret': 'SeCrEtKeYfOrHaShInG',
     'federation': {
         'naver': {
-            'client_id': '',
-            'secret_id': '',
-            'callback_url': '/auth/login/naver/callback'
-        },
-        'facebook': {
-            'client_id': '',
-            'secret_id': '',
-            'callback_url': '/auth/login/facebook/callback'
+            'clientID': '',
+            'clientSecret': '',
         },
         'kakao': {
-            'client_id': '',
-            'callback_url': '/auth/login/kakao/callback'
+            'clientID': '',
         }
     }
 }
