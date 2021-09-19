2c2096c56dc7a77106073593dd885c58874f979d
JSC <gbs04087@gmail.com>
2021-09-19 20:15:52 +0900

    modified kakao strategy - passport

--- backend/Common/passport/index.js
+++ backend/Common/passport/index.js
@@ -90,71 +90,72 @@ module.exports = (passport) => {
                 'callbackURL': '/api/auth/login/kakao/callback',
                 passReqToCallback: true
             },
-            async (req, accessToken, refreshToken, profile, done) => {
-                console.log("profile : ", profile);
-                var _profile = profile._json;
-                const provider = String(profile.provider);
-                const id = String(profile.id);
-                console.log(provider, id)
-                const { user, success } = await getSnsInUser({ id, provider });
-                console.log(user, success)
-                if (success) {
-                    done(null, { nickname: user.nickname, email: user.email });
-                }
-                else {
-                    done("로그인 실패 : 가입 된 계정이 없습니다.");
-                }
-            }
             // async (req, accessToken, refreshToken, profile, done) => {
             //     console.log("profile : ", profile);
-            //     console.log("req.isAuthenticated() : ", req.isAuthenticated())
-            //     // console.log('req : ', req)
-            //     console.log(req.protocol + '://' + req.get('host') + "||||" + req.originalUrl)
-            //     // const _profile = profile._json;
-            //     // const email = String(profile.email);
+            //     var _profile = profile._json;
             //     const provider = String(profile.provider);
             //     const id = String(profile.id);
-            //     // const { id, email } = profile._json;
-            //     console.log(provider, id, email);
-            //     if (isString(provider) &&
-            //         isString(id) &&
-            //         isString(email)) {
-            //         const { user, success } = await getSnsInUser({ id, provider });
-            //         const { duplicate } = await isDuplicateEmail({ email })
-            //         if (success) {
-            //             // sns로 가입된 계정이 있을 때
-            //             done(null, { nickname: user.nickname, email: user.email });
-            //         }
-            //         else {
-            //             // sns로 가입된 계정이 없을 때
-            //             if (!duplicate) {
-            //                 // 이메일이 중복되지 않았다면 가입한다.
-            //                 const { success } = await createUserForSns({
-            //                     user: {
-            //                         email,
-            //                         sns: { provider, id }
-            //                     }
-            //                 })
-            //                 if (success) {
-            //                     // 회원가입 성공이면
-            //                     done(null, { nickname: "", email })
-            //                 }
-            //                 else {
-            //                     // 회원가입 실패이면
-            //                     done("네이버 간편 회원가입 실패", false);
-            //                 }
-            //             }
-            //             else {
-            //                 // 이메일이 중복되었다면 로그인 실패 및 가입불가 안내
-            //                 done("로그인 실패 : 해당 이메일로 가입된 계정이 있습니다.", false);
-            //             }
-            //         }
+            //     console.log(provider, id)
+            //     const { user, success } = await getSnsInUser({ id, provider });
+            //     console.log(user, success)
+            //     if (success) {
+            //         done(null, { nickname: user.nickname, email: user.email });
             //     }
             //     else {
-            //         done("로그인 실패 : 이메일을 받아와야 합니다.");
+            //         done("로그인 실패 : 가입 된 계정이 없습니다.");
             //     }
+            // },
+            async (req, accessToken, refreshToken, profile, done) => {
+                console.log("profile : ", profile);
+                console.log("req.isAuthenticated() : ", req.isAuthenticated())
+                // console.log('req : ', req)
+                console.log(req.protocol + '://' + req.get('host') + "||||" + req.originalUrl)
+                const _profile = profile._json;
+                const email = String(_profile.kakao_account.email);
+                const provider = String(profile.provider);
+                const id = String(profile.id);
+                // const { id, email } = profile._json;
+                console.log(provider, id, email);
+                if (isString(provider) &&
+                    isString(id) &&
+                    _profile.kakao_account.has_email &&
+                    isString(email)) {
+                    const { user, success } = await getSnsInUser({ id, provider });
+                    if (success) {
+                        // sns로 가입된 계정이 있을 때
+                        done(null, { nickname: user.nickname, email: user.email });
+                    }
+                    else {
+                        // sns로 가입된 계정이 없을 때
+                        const { duplicate } = await isDuplicateEmail({ email })
+                        if (!duplicate) {
+                            // 이메일이 중복되지 않았다면 가입한다.
+                            const { success } = await createUserForSns({
+                                user: {
+                                    email,
+                                    sns: { provider, id }
+                                }
+                            })
+                            if (success) {
+                                // 회원가입 성공이면
+                                done(null, { nickname: "", email })
+                            }
+                            else {
+                                // 회원가입 실패이면
+                                done("카카오 간편 회원가입 실패", false);
+                            }
+                        }
+                        else {
+                            // 이메일이 중복되었다면 로그인 실패 및 가입불가 안내
+                            done("로그인 실패 : 해당 이메일로 가입된 계정이 있습니다.", false);
+                        }
+                    }
+                }
+                else {
+                    done("로그인 실패 : 이메일을 받아와야 합니다.");
+                }
 
-            // }
+            }
         )
     );
 
