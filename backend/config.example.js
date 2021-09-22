d3828765477f870000afaef35170f26ffa9fb5fe
Chan <ieejo716@naver.com>
2021-09-22 21:00:44 +0900

    main design

--- backend/config.example.js
+++ backend/config.example.js
@@ -1,13 +1,12 @@
-// change file name config.js
 module.exports = {
     'secret': 'SeCrEtKeYfOrHaShInG',
-    'federation': {
-        'naver': {
-            'clientID': '',
-            'clientSecret': '',
+    'federation':{
+        'naver':{
+            'clientID':'HI9KL4ilNF_j6vWKWQ8P',
+            'clientSecret':'3qcqEpQDQI'
         },
-        'kakao': {
-            'clientID': '',
+        'kakao':{
+            'clientID':'d3319729403e4545bc47b2b7ae249210'
         }
     }
 }
