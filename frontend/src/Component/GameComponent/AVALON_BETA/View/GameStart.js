df9e15b48d85ac5dc0a50247199f730fb43dc85a
Chan <ieejo716@naver.com>
2021-10-07 21:07:59 +0900

    commit

--- frontend/src/Component/GameComponent/AVALON_BETA/View/GameStart.js
+++ frontend/src/Component/GameComponent/AVALON_BETA/View/GameStart.js
@@ -1,23 +1,29 @@
 import React, { useContext } from "react";
 import { GameContext, MINIMUM_PLAYER_NUMBER, START_FRAME } from "../Store";
 import { PeersContext } from "../../../../Routes/peerStore";
-
+import * as S from "../Styled";
+import AVALON_TIMER from "./Timer";
 function GameStart() {
   const { dispatch, gameState } = useContext(GameContext);
   const { peers } = useContext(PeersContext);
 
   console.log(`dispatch : ${dispatch} , gameState : ${gameState}`);
-  const onClick = () => {
-    dispatch({ type: START_FRAME, peers });
-  };
+  // const onClick = () => {
+  //   dispatch({ type: START_FRAME, peers });
+  // };
   return (
     <div>
-      <button
+      <AVALON_TIMER
+        minute={0}
+        seconds={2}
+        callDispatch={dispatch({ type: START_FRAME, peers })}
+      />
+      {/* <button
         disabled={peers.length + 1 < MINIMUM_PLAYER_NUMBER}
         onClick={onClick}
       >
         게임 시작
-      </button>
+      </button> */}
     </div>
   );
 }
